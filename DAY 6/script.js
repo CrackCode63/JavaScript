0f7a201d3b61e70a61605c18824a744387118bff
Abhishek Shakya <abshakya63@GMAIL.COM>
2024-08-14 23:10:55 +0530

    Add files via upload

---
+++ DAY 6/script.js
@@ -0,0 +1,46 @@
+// Expression and Operators
+// ===========================
+// The expression is a simple arithmetic expression with one variable, x. The operators are +, -, \*, /, and ^ (exponentiation).
+let a = 2;
+let b = 3;
+console.log("a + b", a+b);
+console.log("a + b", a-b);
+console.log("a + b", a*b);
+console.log("a + b", a**b);
+console.log("a + b", a/b);
+console.log("a + b", a%b);
+
+// We leant about increment and decrement in previous code.
+// We can also use the ++ and -- operators to increment and decrement variables.
+
+// List the assignment operators:
+// = (assignment)
+// += (addition assignment)
+// -= (subtraction assignment)
+// *= (multiplication assignment)
+// /= (division assignment)
+// %= (modulus assignment)
+// **= (exponentiation assignment)
+
+// List comparison operator
+// == (equal to)
+// != (not equal to)
+// === (strictly equal to)
+// !== (not strictly equal to)
+// > (greater than)
+// < (less than)
+// >= (greater than or equal to)
+// <= (less than or equal to)
+
+// Strict equality and inequality are used to check if two values are the same or not. They are used to check if two values are the same type and value.
+// For example, 1 == "1" is true because the string "1" can be converted to the number 1. But 1 === "1" is false because the string
+// "1" is not the same type as the number 1.
+
+// List logical operator
+// && (logical and)
+// || (logical or)
+// ! (logical not)
+// Logical operators are used to combine multiple conditions. They return a boolean value.
+// For example, a && b returns true if both a and b are true. a || b returns true if either a or b is true. !a returns true if a is false.
+
+
